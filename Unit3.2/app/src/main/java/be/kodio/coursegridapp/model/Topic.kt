9b01acb3ae6de839c20dc221ec478e98f3958bc2
JarodKodio <146439221+JarodKodio@users.noreply.github.com>
2025-03-13 12:49:40 +0100

    Course grid app niet compleet

---
+++ Unit3.2/app/src/main/java/be/kodio/coursegridapp/model/Topic.kt
@@ -0,0 +1,10 @@
+package be.kodio.coursegridapp.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
